3e079e4dd1d6709e7c49153283ffe3ed2a6d1f5e
Jonah <jonahkarew@gmail.com>
2019-05-17 12:42:41 -0400

    updating profile front end

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -39,12 +39,39 @@ function showProfileInfo() {
 }
 
 
+function updateProfile(updatedData){
+//holds user data
+var userData = {
+
+
+ firstName: $("#firstName").val().trim(),
+ location: $("#location").val().trim(),
+ steam: $("#steamID").val().trim(),
+ psn: $("#psnID").val().trim(),
+ xbox: $("#xboxIDs").val().trim()
+}
+ console.log(userData)
+$.ajax({
+  method: "PUT",
+  url: "/api/user/update/",
+  data: updatedData
+})
+
+
+}
+
+
+
+
+
 $(document).ready(function () {
   checkWindowSize();
   $('#saveProfile').on('click', function (event) {
     event.preventDefault();
     $('.toast').toast('show');
+    updateProfile()
   });
+  
   showProfileInfo();
 
 });
